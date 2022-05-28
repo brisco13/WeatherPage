7817d582d7e6531f0c9bbe2319bd7a69d0defcb6
Brisco Arechederra <brisco.arch@gmail.com>
2022-05-28 11:26:10 -0600

    outlined psuedocode

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// get  variables
+
+// Reach out to API
+
+// OnClick place query
+
+// generate card for previous search and place below search bar
+
+// get 5 days of weather info 
+
+// make main section for today's weather
+
+// make 5 cards for forecast
